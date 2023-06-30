bdb555b2ea04b8ab6ca5b7540d2fbf85191e8149
Gustavo Goulart <129804650+gustavo-goulart@users.noreply.github.com>
2023-06-29 21:20:26 -0300

    formularios

---
+++ main.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("form-deposito");
+
+function validaNome(nomeCompleto) {
+  const nomeComoArray = nomeCompleto.split(" ");
+  return nomeComoArray.length >= 2;
+}
+
+form.addEventListener("submit", function (e) {
+  let formEValido = false;
+  e.preventDefault();
+
+  const nomeBeneficiario = document.getElementById("nome-beneficiario");
+  const numeroContaBeneficiario = document.getElementById("numero-conta");
+  const valorDeposito = document.getElementById("valor-deposito");
+  const mensagemSucesso = `Montante de ${valorDeposito.value} depositado para o cliente ${nomeBeneficiario.value} - conta ${numeroContaBeneficiario.value}`;
+
+  formEValido = validaNome(nomeBeneficiario.value);
+  if (formEValido) {
+    alert(mensagemSucesso);
+
+    nomeBeneficiario.value = "";
+    numeroContaBeneficiario.value = "";
+    valorDeposito.value = "";
+  } else {
+    alert("O nome não está completo");
+  }
+});
+
+console.log(form);
